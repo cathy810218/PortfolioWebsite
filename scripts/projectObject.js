8ba438bc174ba0e2d42b907281f73f0957c789ca
cathy810218 <cathy810218@gmail.com>
2017-06-18 20:26:25 -0700

    Add projects icons

--- scripts/projectObject.js
+++ scripts/projectObject.js
@@ -6,6 +6,7 @@ var rawData = [
     category: 'iOS app',
     projectUrl: 'https://www.quickfillkeybaord.com',
     publishedOn: '2016-01-02',
+    icon: '<img class="project-icon" src="images/keyboardIcon.png" alt="icon" />',
     body: '<p>This is a keyboard app<p>'
   },
   {
@@ -13,14 +14,24 @@ var rawData = [
     category: 'iOS app',
     projectUrl: 'https://www.quickfillkeybaord.com',
     publishedOn: '2017-06-10',
+    icon: '<img class="project-icon" src="images/hotdogIcon.png" alt="icon" />',
     body: '<p>This is a hotdog game app<p>'
   },
   {
     title: 'Nebulous',
     category: 'iOS app',
-    projectUrl: 'https://www.quickfillkeybaord.com',
+    projectUrl: 'https://itunes.apple.com/app/id1236248588?mt=8',
     publishedOn: '2017-06-02',
+    icon: '<img class="project-icon" src="images/nebulousIcon.png" alt="icon" />',
     body: '<p>This is a weather app<p>'
+  },
+  {
+    title: 'SimpleTee',
+    category: 'iOS app',
+    projectUrl: 'https://itunes.apple.com/app/id1236248588?mt=8',
+    publishedOn: '2015-12-02',
+    icon: '<img class="project-icon" src="images/simpleTeeIcon.png" alt="icon" />',
+    body: '<p>This is a t-shirt app<p>'
   }
 
 ]
